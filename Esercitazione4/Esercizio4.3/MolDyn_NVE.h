43cb54320942765d13c406edf3c8a99ce293ffba
MassimilianoMatteucci <89901926+MassimilianoMatteucci@users.noreply.github.com>
2021-09-01 12:49:36 +0200

    Add files via upload

---
+++ Esercitazione4/Esercizio4.3/MolDyn_NVE.h
@@ -0,0 +1,56 @@
+/****************************************************************
+*****************************************************************
+    _/    _/  _/_/_/  _/       Numerical Simulation Laboratory
+   _/_/  _/ _/       _/       Physics Department
+  _/  _/_/    _/    _/       Universita' degli Studi di Milano
+ _/    _/       _/ _/       Prof. D.E. Galli
+_/    _/  _/_/_/  _/_/_/_/ email: Davide.Galli@unimi.it
+*****************************************************************
+*****************************************************************/
+using namespace std;
+#include <string> 
+
+//parameters, observables
+const int m_props=4;
+int n_props,count=1;
+int iv,ik,it,ie;
+double stima_pot, stima_kin, stima_etot, stima_temp;
+
+// averages
+double acc,att;
+double mean_pot[3], mean_kin[3], mean_etot[3], mean_temp[3];
+double mean2_pot[2], mean2_kin[2], mean2_etot[2], mean2_temp[2];
+//configuration
+const int m_part=108;
+double x[m_part],y[m_part],z[m_part],xold[m_part],yold[m_part],zold[m_part];
+double vx[m_part],vy[m_part],vz[m_part];
+int Nblocks;
+// thermodynamical state
+int npart;
+double energy,temp,vol,rho,box,rcut;
+
+// simulation
+int nstep, iprint, seed;
+double delta;
+
+//functions
+void Input(bool);
+void Move(void);
+void SetForTemp(double);
+void ConfFinal(void);
+void ConfOld(void);
+void ConfXYZ(int);
+void Measure(void);
+void MeanMeasure(int);
+double Force(int, int);
+void PrintMean(string);
+double Pbc(double);
+/****************************************************************
+*****************************************************************
+    _/    _/  _/_/_/  _/       Numerical Simulation Laboratory
+   _/_/  _/ _/       _/       Physics Department
+  _/  _/_/    _/    _/       Universita' degli Studi di Milano
+ _/    _/       _/ _/       Prof. D.E. Galli
+_/    _/  _/_/_/  _/_/_/_/ email: Davide.Galli@unimi.it
+*****************************************************************
+*****************************************************************/
